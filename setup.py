6e9280e477f2658b233cd7d95bbe7a618585b8d2
Daniel Brandt <me@dbrandt.se>
2014-10-22 12:28:28 +0200

    small setup.py errors.

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+import imp
 import os
 
-from spambayes_lite import __version__
+m_info = imp.find_module("spambayes_lite", [os.path.dirname(__file__)])
+m = imp.load_module("sbl", *m_info)
 
 readme_fname = os.path.join(os.path.dirname(__file__), "README.rst")
 readme_text = open(readme_fname).read()
 
 setup(
     name='spambayes-lite',
-    version = __version__,
+    version = m.__version__,
     description = "Bare-bones spam classification library based on a modified version of SpamBayes.",
     author = "Daniel Brandt",
     author_email = "me@dbrandt.se",
@@ -17,16 +19,16 @@ setup(
     install_requires=["lockfile", "dnspython", "pymongo"],
     long_description=readme_text,
     packages=find_packages(),
-    licence="PSFL",
+    license="PSFL",
     classifiers = [
-        "Development Status :: 4 - Beta"
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: Python Software Foundation License",
         "Operating System :: POSIX",
         "Operating System :: MacOS :: MacOS X",
         "Natural Language :: English",
         "Programming Language :: Python",
         "Intended Audience :: Developers",
-        "Topic :: Software Development :: Libraries"
+        "Topic :: Software Development :: Libraries",
         "Topic :: Text Processing :: Filters"
         ],
     )
